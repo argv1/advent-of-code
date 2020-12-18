dc962413363664ffc47102b700f24fb55dfdd830
argv1 <christian.meyer@hofa.biz>
2020-12-19 00:47:04 +0100

    -

--- 2020/day3a-code.py
+++ 2020/day3a-code.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 '''
-    advent of code 2020 Day 2 Challenge 2
+    advent of code 2020 Day 3 Challenge 1
 '''
 
-import re
-
-res = 0
-with open('H:/OneDrive/Programme/_current/adventofcode/day2-input.txt', 'r') as f:
+res, position = 0, 0
+with open('H:/OneDrive/Programme/_current/adventofcode/2020/day3-input.txt', 'r') as f:  
     for line in f:
-        result = re.search('^(\d+)-(\d+)\s(\w):\s(\w+)', line)
-        if(result.groups()[3][int(result.groups()[0])-1] == result.groups()[2] and result.groups()[3][int(result.groups()[1])-1] != result.groups()[2] or result.groups()[3][int(result.groups()[0])-1] != result.groups()[2] and result.groups()[3][int(result.groups()[1])-1] == result.groups()[2]):
-            res+=1
-    print(res)
+        line = line.rstrip("\n")
+        if(line[position] == '#'): res+=1
+        if(position <= (len(line) -4)):
+            position+= 3        
+        else: 
+            position+= 3-len(line)
+print(res)
