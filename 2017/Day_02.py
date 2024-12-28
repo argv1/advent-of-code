d7658f618ced2a07317f4a4c7b45f5fb6011c52c
argv1 <github@hofa.biz>
2024-12-28 23:28:18 +0100

    combination
    min max instead  and map, no sorting

--- 2017/Day_02.py
+++ 2017/Day_02.py
@@ -3,29 +3,24 @@
 '''
     advent of code 2017 Day 2
 '''
+from itertools import combinations
 
 def main():
+    
     result_a, result_b = 0, 0
-
     with open(r"d:/AOC/2017/Day_02_Input.txt", "r") as f:
         for line in f:
-            line = line.strip().split("\t")
-            line = [ int(x) for x in line]
-            line.sort()
-            result_a += (line[-1] - line[0])
+            line = list(map(int, line.strip().split("\t")))
+            result_a += max(line) - min(line)
             
-            for n in range(len(line)):
-                c+=1
-                found = False
-                for m in range(len(line)):
-                    if line[m] % line[n] == 0 and line[m] / line[n] != 1:
-                        result_b += line[m] / line[n]
-                        found = True
-                        break
-                if found:
+            for a, b in combinations(line, 2):
+                if a % b == 0:
+                    result_b += a // b
+                    break
+                elif b % a == 0:
+                    result_b += b // a
                     break
        
     print(f"{result_a = }\n{result_b = }")
-
 if __name__  == "__main__":
     main()
