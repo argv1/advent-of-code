d8406e0d10707ed07ce7dd95172e1e0876cd6c87
argv1 <github@hofa.biz>
2024-12-28 23:28:39 +0100

    permutation

--- 2017/Day_02.py
+++ 2017/Day_02.py
@@ -3,24 +3,24 @@
 '''
     advent of code 2017 Day 2
 '''
-from itertools import combinations
+
+from itertools import permutations
 
 def main():
-    
     result_a, result_b = 0, 0
+
     with open(r"d:/AOC/2017/Day_02_Input.txt", "r") as f:
         for line in f:
+            c+=1
             line = list(map(int, line.strip().split("\t")))
             result_a += max(line) - min(line)
             
-            for a, b in combinations(line, 2):
+            for a, b in permutations(line, 2):
                 if a % b == 0:
                     result_b += a // b
                     break
-                elif b % a == 0:
-                    result_b += b // a
-                    break
        
     print(f"{result_a = }\n{result_b = }")
+
 if __name__  == "__main__":
     main()
